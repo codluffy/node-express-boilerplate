0fe59b4a18b37212cec154cf87a468081e9232e1
codewithmkrp <143056817+codewithmkrp@users.noreply.github.com>
2023-11-19 09:24:58 +0545

    Modified

--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 function Greetings() {
-  return <h2>My First React App</h2>
+    return (
+        <div>
+            <h2>My First React App</h2>
+            {/*<p>This is a paragraph</p>*/}
+            Enter the number: <br/>
+            <input type="number" />
+        </div>
+    );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
